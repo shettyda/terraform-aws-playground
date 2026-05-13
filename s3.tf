12845a0de96c62d13582b87391ed0fe799ca4fa8
Darshan Shetty <dsdarshan19@gmail.com>
2026-05-13 17:54:14 +0000

    Indentation done

--- s3.tf
+++ s3.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "my_bucket" {
 
-   bucket = "shetty-terra-bucket"
+  bucket = "shetty-terra-bucket"
 
-tags = {
-   Name = "terra-created-bucket"
-   Environment = "Test Lab"
-   Owner = "shettyda"
-}
+  tags = {
+    Name        = "terra-created-bucket"
+    Environment = "Test Lab"
+    Owner       = "shettyda"
+  }
 
 }
--- vm.tf
+++ vm.tf
@@ -9,9 +9,9 @@ resource "aws_uxc_account_customizations" "account_ui" {
 # VPC Creation
 
 resource "aws_vpc" "terra_vpc" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-tags ={
+  tags = {
     Name = "Terra-created-VPC"
   }
 }
@@ -19,73 +19,73 @@ tags ={
 # Internet Gateway creation
 
 resource "aws_internet_gateway" "terra_gw" {
-     vpc_id = aws_vpc.terra_vpc.id             #interpolation
+  vpc_id = aws_vpc.terra_vpc.id #interpolation
 
-tags ={
-    Name ="Terra-Gateway"
-}
+  tags = {
+    Name = "Terra-Gateway"
+  }
 }
 # Public Subnet Creation
 
 resource "aws_subnet" "pub_subnet" {
-    cidr_block = "10.0.1.0/24"
-    vpc_id = aws_vpc.terra_vpc.id
+  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.terra_vpc.id
 
-tags = {
+  tags = {
     Name = "Terra-Subnet-public-"
   }
 }
 
 # Private subnet Creation
 
-resource "aws_subnet" "pri_subnet"{
-    cidr_block = "10.0.2.0/24"
-    vpc_id = aws_vpc.terra_vpc.id
+resource "aws_subnet" "pri_subnet" {
+  cidr_block = "10.0.2.0/24"
+  vpc_id     = aws_vpc.terra_vpc.id
 
-tags = {
+  tags = {
     Name = "Terra-subnet-Private"
-  
-}
+
+  }
 }
 
 #Public Route Table Creation 
 
-resource "aws_route_table" "pub_terra_rt"{
-    vpc_id = aws_vpc.terra_vpc.id
+resource "aws_route_table" "pub_terra_rt" {
+  vpc_id = aws_vpc.terra_vpc.id
 
-    route {
-        cidr_block ="0.0.0.0/0"
-        gateway_id = aws_internet_gateway.terra_gw.id
-        
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terra_gw.id
 
-tags = {
+  }
+
+  tags = {
     Name = "Terra-Route-Table-Public"
-}
+  }
 }
 
 
 # Private route Table creation
 
-resource "aws_route_table" "priv_terra_rt"{
-    vpc_id = aws_vpc.terra_vpc.id
+resource "aws_route_table" "priv_terra_rt" {
+  vpc_id = aws_vpc.terra_vpc.id
 
-tags = {
+  tags = {
     Name = "Terra-Route-Table-Private"
-} 
+  }
 }
 
 # Associate Route Table with Subnet
 
-resource "aws_route_table_association" "rt_ast"{
-    subnet_id = aws_subnet.pub_subnet.id
-    route_table_id = aws_route_table.pub_terra_rt.id
+resource "aws_route_table_association" "rt_ast" {
+  subnet_id      = aws_subnet.pub_subnet.id
+  route_table_id = aws_route_table.pub_terra_rt.id
 }
 
 # Associate Route Table with Private Subnet
 
-resource "aws_route_table_association" "pri_rt_ast"{
-    subnet_id = aws_subnet.pri_subnet.id
-    route_table_id = aws_route_table.priv_terra_rt.id
+resource "aws_route_table_association" "pri_rt_ast" {
+  subnet_id      = aws_subnet.pri_subnet.id
+  route_table_id = aws_route_table.priv_terra_rt.id
 }
   
